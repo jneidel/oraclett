ca39ca5f925b08ccf9e9eff4f52fe07b744f9685
Jonathan Neidel <jneidel@jneidel.com>
2023-01-05 11:32:25 +0100

    Add hours list command

---
+++ src/commands/hours/list.ts
@@ -0,0 +1,30 @@
+import { Command, Flags } from "@oclif/core";
+import { listHours } from "../../controller/hours";
+
+export default class List extends Command {
+  static description = "List all projects.";
+
+  static flags = {
+    date: Flags.string( {
+      char       : "d",
+      description: "A date to specify the week (defaults to this week, can be human-readable)",
+      default    : "today",
+    } ),
+    short: Flags.boolean( {
+      char       : "s",
+      description: "Show shortend titles for the projects (for smaller terminals)",
+      default    : false,
+    } ),
+  };
+
+  static examples = [ `$ <%= config.bin %> <%= command.id %>
+$ <%= config.bin %> <%= command.id %> -d "last week" --short
+` ];
+
+  async run(): Promise<void> {
+    const { flags } = await this.parse( List );
+    const { date, short } = flags;
+
+    listHours( date, short );
+  }
+}
--- src/controller/hours.ts
+++ src/controller/hours.ts
@@ -1,7 +1,8 @@
-import chalk from "chalk";
+import { CliUx } from "@oclif/core";
 const { Date } = require( "sugar" );
 import { fs, HOURS_FILE } from "../config";
-import { parseOracleString, combineToOracleString } from "./utils";
+import { combineToOracleString } from "./utils";
+import { readProjects } from "./project";
 
 export const readHours = async ( forceReadingFromDisk = false ) => fs.read( HOURS_FILE, forceReadingFromDisk );
 const writeHours = async data => fs.write( HOURS_FILE, data );
@@ -55,4 +56,113 @@ export async function addHours( data: {
   await writeHours( hours );
 
   // print out weekly hours logged for that combination
+}
+
+export async function listHours( dateStringOrWeekNr: string|number, shortendTitles ) {
+  let date;
+  if ( typeof dateStringOrWeekNr === "number" ) {
+    // date = new Date();
+    // date.setISOWeek(); broken
+  } else {
+    date = Date.create( dateStringOrWeekNr );
+  }
+  const [ isoWeek, isoYear ] = Date.format( date, "%V %G" ).split( " " );
+
+  const relevantHours = await readHours()
+    .then( hours => hours[isoYear][isoWeek] )
+    .catch( () => null );
+
+  if ( relevantHours === null ) {
+    console.log( `No hours logged in week ${isoWeek} of ${isoYear}.
+
+To log some use: hours add` );
+  } else {
+    const projects = Object.keys( relevantHours );
+    const projectTaskDetailCombinations = projects.reduce( ( acc, project ) => {
+      const taskDetails = Object.keys( relevantHours[project] );
+      taskDetails.forEach( tdKey => {
+        // @ts-ignore
+        acc.push( {
+          project,
+          taskDetail: tdKey,
+        } );
+      } );
+      return acc;
+    }, [] );
+
+    const totals = { total: 0, Mon: 0, Tue: 0, Wed: 0, Thu: 0, Fri: 0, Sat: 0, Sun: 0 };
+    function calulcateTotal( combiHours ) {
+      const total = Object.keys( combiHours ).reduce( ( acc, key ) => {
+        const hours = combiHours[key];
+        if ( typeof hours === "number" ) {
+          totals[key] += hours;
+          return acc + hours;
+        } else {return acc;}
+      }, 0 );
+      totals.total += total;
+      return total;
+    }
+
+    let hasWeekend = false;
+    const hoursPerWeekday = projectTaskDetailCombinations.map( combi => {
+      const { project, taskDetail } = combi;
+      const combiHours = relevantHours[project][taskDetail];
+
+      if ( !hasWeekend )
+        hasWeekend = !!( combiHours.Sat || combiHours.Sun );
+
+      const total = calulcateTotal( combiHours );
+
+      return {
+        Mon: combiHours.Mon,
+        Tue: combiHours.Tue,
+        Wed: combiHours.Wed,
+        Thu: combiHours.Thu,
+        Fri: combiHours.Fri,
+        Sat: combiHours.Sat,
+        Sun: combiHours.Sun,
+        total,
+      };
+    } );
+
+    const rawProjectsData = await readProjects();
+    const projectTaskDetailText = projectTaskDetailCombinations.map( ( combi: any ) => {  // TODO: include descriptions
+      if ( shortendTitles ) {return `${combi.project}: ${combi.taskDetail} `;} else {
+        const rawProject = rawProjectsData[combi.project];
+        const projectText = combineToOracleString( combi.project, rawProject.description );
+        const taskDetailText = combineToOracleString( combi.taskDetail, rawProject.taskDetails[combi.taskDetail].description );
+
+        return `${projectText}: ${taskDetailText} `;
+      }
+    } );
+
+    const tableData  = projectTaskDetailText.map( ( project, index ) => {
+      const hours = hoursPerWeekday[index];
+      return Object.assign( { project }, hours );
+    } );
+
+    Object.keys( totals ).forEach( key => {
+      if ( totals[key] === 0 )
+        totals[key] = undefined;
+    } );
+    tableData.push( Object.assign( { project: "Sums" }, totals ) );
+
+    const columns: any = Object.assign(
+      {
+        project: {
+          header: "Project: Task Details",
+        },
+        Mon: {},
+        Tue: {},
+        Wed: {},
+        Thu: {},
+        Fri: {},
+      },
+      hasWeekend ? { Sat: {}, Sun: {} } : {},
+      { total: {} }
+    );
+
+    console.log( `Logged hours for week ${isoWeek} of ${isoYear}:\n` );
+    CliUx.ux.table( tableData, columns, {} );
+  }
 }
