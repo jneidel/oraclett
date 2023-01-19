8576e7c448abc76c863b5432a32282c1526cda8e
Jonathan Neidel <jneidel@jneidel.com>
2023-01-19 07:45:49 +0100

    Fix data paths for windows

--- src/config/data-directory.ts
+++ src/config/data-directory.ts
@@ -1,4 +1,12 @@
-export const DATA_DIR = `${process.env.HOME}/.local/share/oraclett`;
-export const PROJECTS_FILE = `${DATA_DIR}/projects.json`;
-export const HOURS_FILE = `${DATA_DIR}/hours.json`;
-export const TASKS_FILE = `${DATA_DIR}/tasks.json`;
+import { join } from "path";
+
+const { HOME } = process.env;
+if ( !HOME ) {
+  console.error( "Environmental variable $HOME is not set" );
+  process.exit( 1 );
+}
+
+export const DATA_DIR = join( HOME, ".local/share/oraclett" );
+export const PROJECTS_FILE = join( DATA_DIR, "projects.json" );
+export const HOURS_FILE = join( DATA_DIR, "hours.json" );
+export const TASKS_FILE = join( DATA_DIR, "tasks.json" );
