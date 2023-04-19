20f3dd118e2dbe99696eab95b066cf89d1b002b9
Jonathan Neidel <jneidel@jneidel.com>
2023-04-19 09:54:36 +0200

    Add seperate dev data directory

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,11 @@
-export const DATA_DIR = `${process.env.HOME}/.local/share/oraclett`;
+export let DATA_DIR: string;
+if ( process.argv[1].match( /bin\/run$/ ) )
+  // dev
+  DATA_DIR = `${process.env.HOME}/.local/share/oraclett-dev`;
+else
+  // prod
+  DATA_DIR = `${process.env.HOME}/.local/share/oraclett`;
+
 export const PROJECTS_FILE = `${DATA_DIR}/projects.json`;
 export const HOURS_FILE = `${DATA_DIR}/hours.json`;
 export const NOTES_FILE = `${DATA_DIR}/notes.json`;
